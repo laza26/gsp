b97a1fb54ffb17f2c1021ac0d8fd52b1d037b50e
laza26 <laza26@gmail.com>
2012-12-10 01:22:18 +0100

    Dodate dve test strane da vidis kako ce funkcionisati njihovo pozivanje u content

--- galerija.php
+++ galerija.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>ГСП Београд | Галерија</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="css/styles_2cl.css" />
-<link rel="shortcut icon" href="images/icon.png">
+<link rel="shortcut icon" href="images/icon.png" />
 	
 	<!-- lightbox -->
 	<link href="css/lightbox.css" rel="stylesheet" />
