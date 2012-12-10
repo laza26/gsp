8f64b2fe14017dc7f25ab491c93e3c49b03c3296
laza26 <laza26@gmail.com>
2012-12-10 16:27:43 +0100

    Galerija prilagodjena da se ucitava u index.php-u, resena ul-li umesto tabelom, obrisane dve test strane koje su postojale, sa index-a sklonjen sav sadrzaj tako sto je zakomentarisan samo u fajlu

--- index.php
+++ index.php
@@ -5,11 +5,18 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 	<link rel="shortcut icon" href="images/icon.png" />
+        <script src="js/jquery-1.7.2.min.js"></script>
+        
+        <!-- lightbox -->
+	<link href="css/lightbox.css" rel="stylesheet" />
+	
+	<script src="js/lightbox.js"></script>
+	<!-- end lightbox -->
 	
 	<!-- Nivo Slider -->
 	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="themes/default/default.css" type="text/css" media="screen" />
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+	<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>-->
 	<script src="js/jquery.nivo.slider.pack.js" type="text/javascript"></script>
 	<!-- End of Slider -->
         
@@ -17,7 +24,7 @@
 		<link href="css/dropdown/dropdown.css" media="screen" rel="stylesheet" type="text/css" />
 		<link href="css/dropdown/themes/default/default.advanced.css" media="screen" rel="stylesheet" type="text/css" />
 	<!-- / END -->
-	
+        	
 	<link href="css/menu_assets/styles.css" rel="stylesheet" type="text/css" />
 	
 	<!-- Moji meniji -->
@@ -58,10 +65,6 @@
   </div>
   <!-- End Header -->
   
-<!--   pomocni 
-  <div id="pomocni">
-  </div>
-   end pomocni-->
   
   <!-- Begin Naviagtion -->
   <div id="navigation">
@@ -90,7 +93,7 @@
 				<li><a href="skolske_linije.php">Школске линије</a></li>
 			</ul>
 		</li>
-		<li><a href="galerija.php">Галерија</a></li>
+		<li><a href="?strana=galerija">Галерија</a></li>
 		<li class="dir">Карте
 			<ul>
 				<li><a href="karte_i_markice.php">Карте и маркице</a></li>
@@ -148,30 +151,37 @@ if(!$rezulat2){
    exit;
 }
 
-while($ispis1=mysql_fetch_array($rezulat1))
-{
-	echo "<h3><center>".$ispis1['Sadrzaj']."</center></h3></br></br>";
-}
+//while($ispis1=mysql_fetch_array($rezulat1))
+//{
+//	echo "<h3><center>".$ispis1['Sadrzaj']."</center></h3></br></br>";
+//}
 mysql_close($con);
 ?>
-<br><center>
+<!--<br><center>
 <table id="veliki_meni" border=0 cellspacing="4">
 	<tr><td><a href="dnevne_linije.php" target="_blank"><img src="images/meni_naslovna/dnevne_linije.png" name="dnevne_linije" alt="Дневне линије" onMouseover="document.dnevne_linije.src=dnevne_linije_over.src;" onClick="document.dnevne_linije.src=dnevne_linije.src;" onMouseout="document.dnevne_linije.src=dnevne_linije_default.src;" style="border:0;"></a></td>
 		<td><a href="nocne_linije.php" target="_blank"><img src="images/meni_naslovna/nocne_linije.png" name="nocne_linije" alt="Ноћне линије" onMouseover="document.nocne_linije.src=nocne_linije_over.src;" onClick="document.nocne_linije.src=nocne_linije.src;" onMouseout="document.nocne_linije.src=nocne_linije_default.src;" style="border:0;"></a></td>
 		<td><a href="karte_i_markice.php" target="_blank"><img src="images/meni_naslovna/karte_i_markice.png" name="karte_i_markice" alt="Карте и маркице" onMouseover="document.karte_i_markice.src=karte_i_markice_over.src;" onClick="document.karte_i_markice.src=karte_i_markice.src;" onMouseout="document.karte_i_markice.src=karte_i_markice_default.src;" style="border:0;"></a></td>
 		<td><a href="promena_rezima.php" target="_blank"><img src="images/meni_naslovna/izmene_rezima.png" name="izmene_rezima" alt="Измене режима" onMouseover="document.izmene_rezima.src=izmene_rezima_over.src;" onClick="document.izmene_rezima.src=izmene_rezima.src;" onMouseout="document.izmene_rezima.src=izmene_rezima_default.src;" style="border:0;"></td>
 	</tr>
-</table><br><br><br><br><br>
+</table><br><br><br><br><br>-->
 
 <?php
-while ($ispis2=mysql_fetch_array($rezulat2))
-{
-	echo"<h4>".$ispis2['Sadrzaj']."</h4><br><br>";
-}
+//while ($ispis2=mysql_fetch_array($rezulat2))
+//{
+//	echo"<h4>".$ispis2['Sadrzaj']."</h4><br><br>";
+//}
 ?>
-<a href="http://www.bgsaobracaj.rs" border=0 target=blank><img src="images/banners/szs.jpg"></a> &nbsp
-<a href="http://www.uitp.org" border=0 target=blank><img src="images/banners/logo_UITP.gif" width="75px" height="58px"></a>
+<!--<a href="http://www.bgsaobracaj.rs" border=0 target=blank><img src="images/banners/szs.jpg"></a> &nbsp
+<a href="http://www.uitp.org" border=0 target=blank><img src="images/banners/logo_UITP.gif" width="75px" height="58px"></a>-->
 </center>
+
+<?php
+error_reporting (E_ALL ^ E_NOTICE);
+    if ($_REQUEST ['strana']=='galerija')
+        include ($_REQUEST['strana'] . '.php');
+?>
+
   </div>
   <!-- End Content -->
   
