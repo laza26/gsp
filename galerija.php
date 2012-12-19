53e701611089176aeab731827bb0d21721533d68
laza26 <laza26@gmail.com>
2012-12-19 17:02:26 +0100

    Dodat box shadow na slike u galeriji

--- galerija.php
+++ galerija.php
@@ -3,16 +3,16 @@
             ?>
             
 		<ul class="galerija">
-			<li><a href="galeries/galerija/bus_01.jpg" rel="lightbox[galerija]" title="Аутобус MAN, 'Јапанац'"><img src='galeries/galerija/bus_01_thumb.png' /></a></li>
-			<li><a href="galeries/galerija/tramvaj_02.jpg" rel="lightbox[galerija]" title="Швајцарски трамвај"><img src='galeries/galerija/tramvaj_02_thumb.png' /></a></li>
-			<li><a href="galeries/galerija/trola_01.jpg" rel="lightbox[galerija]" title="Тролејбуси"><img src='galeries/galerija/trola_01_thumb.png' /></a></li>
-			<li><a href="galeries/galerija/bus_03.jpg" rel="lightbox[galerija]" title="Аутобус"><img src='galeries/galerija/bus_03_thumb.png'/></a></li>		
-			<li><a href="galeries/galerija/tramvaj_01.jpg" rel="lightbox[galerija]" title="Трамвај"><img src='galeries/galerija/tramvaj_01_thumb.png'/></a></li>
-			<li><a href="galeries/galerija/bus_02.jpg" rel="lightbox[galerija]" title="Аутобус"><img src='galeries/galerija/bus_02_thumb.png'/></a></li>
-			<li><a href="galeries/galerija/tramvaj_03.jpg" rel="lightbox[galerija]" title="Трамвај"><img src='galeries/galerija/tramvaj_03_thumb.png'/></a></li>
-			<li><a href="galeries/galerija/trola_02.jpg" rel="lightbox[galerija]" title="Тролејбус"><img src='galeries/galerija/trola_02_thumb.png'/></a></li>
-			<li><a href="galeries/galerija/tramvaj_04.jpg" rel="lightbox[galerija]" title="Трамвај 'Шпанац'"><img src='galeries/galerija/tramvaj_04_thumb.png'/></a></li>
-			<li><a href="galeries/galerija/trola_03.jpg" rel="lightbox[galerija]" title="Тролејбус"><img src='galeries/galerija/trola_03_thumb.png'/></a></li>
-			<li><a href="galeries/galerija/bus_04.jpg" rel="lightbox[galerija]" title="Аутобус"><img src='galeries/galerija/bus_04_thumb.png'/></a></li>
-			<li><a href="galeries/galerija/trola_04.jpg" rel="lightbox[galerija]" title="Тролејбус"><img src='galeries/galerija/trola_04_thumb.png'/></a></li>
+			<li><a href="galeries/galerija/bus_01.jpg" rel="lightbox[galerija]" title="Аутобус MAN, 'Јапанац'" class="shadow"><img src='galeries/galerija/bus_01_thumb.png' /></a></li>
+			<li><a href="galeries/galerija/tramvaj_02.jpg" rel="lightbox[galerija]" title="Швајцарски трамвај" class="shadow"><img src='galeries/galerija/tramvaj_02_thumb.png' /></a></li>
+			<li><a href="galeries/galerija/trola_01.jpg" rel="lightbox[galerija]" title="Тролејбуси" class="shadow"><img src='galeries/galerija/trola_01_thumb.png' /></a></li>
+			<li><a href="galeries/galerija/bus_03.jpg" rel="lightbox[galerija]" title="Аутобус" class="shadow"><img src='galeries/galerija/bus_03_thumb.png'/></a></li>		
+			<li><a href="galeries/galerija/tramvaj_01.jpg" rel="lightbox[galerija]" title="Трамвај" class="shadow"><img src='galeries/galerija/tramvaj_01_thumb.png'/></a></li>
+			<li><a href="galeries/galerija/bus_02.jpg" rel="lightbox[galerija]" title="Аутобус" class="shadow"><img src='galeries/galerija/bus_02_thumb.png'/></a></li>
+			<li><a href="galeries/galerija/tramvaj_03.jpg" rel="lightbox[galerija]" title="Трамвај" class="shadow"><img src='galeries/galerija/tramvaj_03_thumb.png'/></a></li>
+			<li><a href="galeries/galerija/trola_02.jpg" rel="lightbox[galerija]" title="Тролејбус" class="shadow"><img src='galeries/galerija/trola_02_thumb.png'/></a></li>
+			<li><a href="galeries/galerija/tramvaj_04.jpg" rel="lightbox[galerija]" title="Трамвај 'Шпанац'" class="shadow"><img src='galeries/galerija/tramvaj_04_thumb.png'/></a></li>
+			<li><a href="galeries/galerija/trola_03.jpg" rel="lightbox[galerija]" title="Тролејбус" class="shadow"><img src='galeries/galerija/trola_03_thumb.png'/></a></li>
+			<li><a href="galeries/galerija/bus_04.jpg" rel="lightbox[galerija]" title="Аутобус" class="shadow"><img src='galeries/galerija/bus_04_thumb.png'/></a></li>
+			<li><a href="galeries/galerija/trola_04.jpg" rel="lightbox[galerija]" title="Тролејбус" class="shadow"><img src='galeries/galerija/trola_04_thumb.png'/></a></li>
 		</ul>
