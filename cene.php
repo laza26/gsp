6f39137c9e6386e8b3f5186639b768a8e23861b9
laza26 <laza26@gmail.com>
2012-12-27 01:17:31 +0100

    Sredjeni stilovi za stranicu Cene

--- cene.php
+++ cene.php
@@ -12,14 +12,14 @@
     <h1>Цене карата и претплатних легитимација</h1>
     <h2>Појединачне карте</h2>
     <h3>Категорија:</h3>		        
-    <select onchange="prikazi_opcije(this.value)">
-        <option value='0'>Молимо одаберите опцију</option>
+    <select onchange="prikazi_opcije(this.value)" class="cene">
+        <option value='0'>Молимо одаберите опцију...</option>
         <?php
         foreach ($karte as $key => $val)
             echo "<option value='$key'>$key</option>";
         ?>
-    </select>
-    <select onchange="" id="opcije"></select>
+    </select> 
+    <select onchange="" id="opcije" class="cene"></select>
     
     <div id="zona_i_cena"></div>
 </div>
@@ -32,7 +32,7 @@
         jQuery('#opcije').children().remove();
         jQuery('#zona_i_cena').children().remove();
         
-        jQuery('#opcije').append("<option value='0'>Молимо одаберите опцију</option>");
+        jQuery('#opcije').append("<option value='0'>Молимо одаберите опцију...</option>");
         for(i = 0; i<karte[vrednost].length; i++) {
             if(prethodna_vrednost != karte[vrednost][i][2]) {
                 prethodna_vrednost = karte[vrednost][i][2];
@@ -45,7 +45,7 @@
         jQuery('#zona_i_cena').children().remove();
         for(i = 0; i<karte[kljuc].length; i++) {
             if(karte[kljuc][i][2] == vrednost) {
-                jQuery('#zona_i_cena').append('<span>Зона: ' + karte[kljuc][i][3] + '<br/>Цена са ПДВ-ом: ' + karte[kljuc][i][4] + ' РСД</span><br/><br/>');
+                jQuery('#zona_i_cena').append('<span><b>Зона: </b>' + karte[kljuc][i][3] + '<br/><b>Цена са ПДВ-ом: </b>' + karte[kljuc][i][4] + ' РСД</span><br/><br/>');
             }
         }
     }
