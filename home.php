a41d246f2da218ede97d2d07d5437fd274e3c182
laza26 <laza26@gmail.com>
2013-01-06 15:04:35 +0100

    Doradjen sadrzaj naslovne strane, ubacena slika i malo sredjeni banneri u sidebar-u

--- home.php
+++ home.php
@@ -1,19 +1,30 @@
 <div id="content_home">
-<?php
+    <?php
     konekcija();
-    $sql="SELECT * FROM `stranice` WHERE ID=1";
-    $result=mysql_query($sql);
-    
-    while($ispis=mysql_fetch_array($result)){
+    $sql = "SELECT * FROM `stranice` WHERE ID=1";
+    $result = mysql_query($sql);
+
+    while ($ispis = mysql_fetch_array($result)) {
         echo $ispis['Tekst'];
     }
-?>
+    ?>
+    <ul class="sredina">
+        <li>
+            <a href="images/meni_naslovna/novi_autobusi.jpg" rel="lightbox[naslovna]" title="GSP"><img src="images/meni_naslovna/novi_autobusi_thumb.png" /></a>
+        </li>
+    </ul>    
 </div>
 
 <div id="sidebar_home">
-<?php
+    <?php
     include 'headlines.php';
-?>
-<a href="http://www.bgsaobracaj.rs" border=0 target=blank><img src="images/banners/szs.jpg"></a></br>
-<a href="http://www.uitp.org" border=0 target=blank><img src="images/banners/logo_UITP.gif" width="75px" height="58px"></a>
+    ?>
+    <ul class="baneri">
+        <li>
+            <a href="http://www.bgsaobracaj.rs" target="_blank"><img src="images/banners/szs.jpg" /></a>
+        </li>
+        <li>
+            <a href="http://www.uitp.org" target="_blank"><img src="images/banners/logo_UITP.gif" class="uitp"/></a>
+        </li>
+    </ul>
 </div>
