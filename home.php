4ef7d41f24ce35bc3b0b1dd3d88f55b69fbeed0e
laza26 <laza26@gmail.com>
2013-01-09 01:41:43 +0100

    Dodatno prilagodjeni za responsive home, galerija, tabele linija, ne i same stranice linija, slike na home, itd

--- home.php
+++ home.php
@@ -8,23 +8,18 @@
         echo $ispis['Tekst'];
     }
     ?>
-    <ul class="sredina">
-        <li>
-            <a href="images/meni_naslovna/novi_autobusi.jpg" rel="lightbox[naslovna]" title="GSP"><img src="images/meni_naslovna/novi_autobusi_thumb.png" /></a>
-        </li>
-    </ul>    
+    <div id="sredina">      
+        <a href="images/meni_naslovna/novi_autobusi.jpg" rel="lightbox[naslovna]" title="GSP"><img src="images/meni_naslovna/novi_autobusi_thumb.png"/></a>
+    </div>    
 </div>
 
 <div id="sidebar_home">
     <?php
     include 'headlines.php';
     ?>
-    <ul class="baneri">
-        <li>
-            <a href="http://www.bgsaobracaj.rs" target="_blank"><img src="images/banners/szs.jpg" /></a>
-        </li>
-        <li>
-            <a href="http://www.uitp.org" target="_blank"><img src="images/banners/logo_UITP.gif" class="uitp"/></a>
-        </li>
-    </ul>
+        <ul class="baneri">
+            <li>
+                <a href="http://www.bgsaobracaj.rs" target="_blank"><img src="images/banners/szs.jpg" /></a>
+            </li>
+        </ul>  
 </div>
